b0d585ec260f0f9714de8187ef826875843a8e5d
Nick Croft <nick@copyblogger.com>
2016-05-18 16:25:05 -0400

    Updating version for WP release

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
   Plugin Name: Genesis Portfolio Pro
   Plugin URI:
   Description: Adds default portfolio to any Genesis HTML5 theme.
-  Version: 1.0.0
+  Version: 1.0
   Author: copyblogger
   Author URI: http://www.copyblogger.com
   Text Domain: genesis-portfolio-pro
