d8f138c23ee1d2db70065897ef1d335133648844
Nick Croft <nick@copyblogger.com>
2017-08-24 08:06:07 -0400

    adds brackets for ternary
    addresses #18

--- lib/templates/taxonomy-portfolio-type.php
+++ lib/templates/taxonomy-portfolio-type.php
@@ -21,7 +21,7 @@ function genesis_portfolio_taxonomy_template_layout( $layout ) {
 	global $wp_query;
 
 	$term   = $wp_query->get_queried_object();
-	$layout = $term && isset( $term->term_id ) && $opt = get_term_meta( $term->term_id, 'layout', true ) ? $opt : __genesis_return_full_width_content();
+	$layout = ( $term && isset( $term->term_id ) && $opt = get_term_meta( $term->term_id, 'layout', true ) ) ? $opt : __genesis_return_full_width_content();
 
 	return $layout;
 
